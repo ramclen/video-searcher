236433cb910c0da77a94a019d4d200a6fa51464c
Cristopher <cristopher.correo@gmail.com>
2020-08-18 12:32:28 +0100

    added travis config to project

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
-const key = 'AIzaSyAx7lbN4vlyWxgrmGeRIfXKob0mrbpMZGI';
+const key = 'AIzaSyAPdr_cyCmEjig_xgnEgTw1wZ_MUZIJQIA';
 
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {
-    part: 'snippet',
-    maxResult: 5,
-    type: 'video',
-    key
-  }
-})
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		maxResult: 5,
+		type: 'video',
+		key
+	}
+});
